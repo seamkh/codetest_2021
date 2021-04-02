c56b5c5b855803851b651f905903e963a8850774
seamkh <seamkh@gmail.com>
2021-04-02 15:42:21 +0900

    Create py_uniqueCharacter.py

---
+++ python/01. uniquecharacter/py_uniqueCharacter.py
@@ -0,0 +1,24 @@
+def isUniqueChars(s):
+    
+    if(len(s) > 256):
+        return False
+    
+    char_set = [False]*256
+
+    for i in range(0,len(s)):
+            #string to ascii
+            val = ord(s[i])
+            print(s[i])
+            print(val)
+            print(char_set[i])
+            if(char_set[val]):
+                print('contain same character')
+                return False;
+            
+            char_set[val] = True
+
+    print('not contain same character')
+    return True
+
+#test function
+isUniqueChars('hello')
