713ce6a9260a96e2dd4a000c9f02be5817f76738
seamkh <seamkh@gmail.com>
2021-04-06 14:07:20 +0900

    Create reverse.py
    문자열 뒤집기

---
+++ python/1.2 reverse/reverse.py
@@ -0,0 +1,10 @@
+#version 3.9.2
+#null 문자로 끝나는 문자열 뒤집는 reverse 함수 
+def reverse (str):
+        print(str)
+        reversed = str[::-1]
+        print(reversed)
+        
+
+#test reverse
+reverse('abcde')
