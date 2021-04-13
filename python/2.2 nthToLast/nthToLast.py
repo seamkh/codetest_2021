684bd260e08aff6f28ce0486c80e0f40591c3e9a
seamkh <seamkh@gmail.com>
2021-04-13 12:07:47 +0900

    연결 리스트에서 뒤에서 k 번째 원소 찾는다

---
+++ python/2.2 nthToLast/nthToLast.py
@@ -0,0 +1,21 @@
+from linkedList import *
+
+def nthToLast(head,k):
+    if head is None :
+        return 0
+
+    i = nthToLast(head.next,k) + 1
+    if i == k :
+        print(head.data)
+        print('index = ' + str(i))
+    return i
+
+a = LinkedList()
+a.insert('a')
+a.insert('b')
+a.insert('c')
+a.insert('d')
+a.insert('e')
+
+nthToLast(a.head,3)
+
